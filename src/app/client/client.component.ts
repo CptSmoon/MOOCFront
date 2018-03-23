c16d9077cedc2a848d083da8dd5dcab07cc09c0c
Yessine Borchani <borchaniz96@gmail.com>
2018-03-23 12:29:29 +0100

    add client done, still some minor bugs to be fixed in the review phase

--- src/app/client/client.component.ts
+++ src/app/client/client.component.ts
@@ -27,10 +27,10 @@ export class ClientComponent implements OnInit {
   constructor(private clientService:ClientService, private regionService:RegionService) {}
 
   ngOnInit() {
+    this.toAddClient=new Client();
     this.getClients();
     this.getTypes();
     this.getVilles();
-    this.toAddClient=new Client();
 
   }
 
@@ -41,31 +41,56 @@ export class ClientComponent implements OnInit {
   public getTypes(){
     this.clientService.getTypes().subscribe(data=>{
       this.types=data;
-      if (this.types) this.toAddClient.type=this.types[0];
+      this.toAddClient.type=this.types[0];
     });
   }
 
   public getVilles(){
     this.regionService.getAll().subscribe(data=>{
       this.villes=data;
-      if (this.villes) this.selectedVille=this.villes[0];
+      this.selectedVille=this.villes[0];
       if (this.villes && this.villes[0].region) this.toAddClient.region=this.villes[0].region[0];
     });
   }
 
   public cleanAddClientModal(){
-    this.toAddClient=new Client();
+    jQuery('#add-client-modal').modal('toggle');
     if (this.types) this.toAddClient.type=this.types[0];
     if (this.villes[0].region) this.toAddClient.region=this.villes[0].region[0];
     if (this.villes) this.selectedVille=this.villes[0];
     if (this.types) this.toAddClient.type=this.types[0];
-    jQuery('#add-client-modal').modal('toggle');
   }
   validChampsClient() {
-    if (this.toAddClient.name && this.toAddClient.mobile && this.toAddClient.email) {
+    if (this.toAddClient.name && this.toAddClient.mobile && this.toAddClient.email&& this.toAddClient.region && this.toAddClient.type&&this.selectedVille) {
       return true;
     }
     return false;
+  }
+
+  addClient(){
+    this.toAddClient.region_id=this.toAddClient.region.region_id;
+    this.toAddClient.type_client_id=this.toAddClient.type.type_client_id;
+    this.clientService.addClient(this.toAddClient).subscribe(data=>{
+      this.clients.push(data);
+        swal({
+          title: "Succès",
+          text: "Le client \""+data.name+"\" a été ajoutée",
+          confirmButtonColor: "#66BB6A",
+          type:"success",
+          button: "OK!",
+        });
+      },
+      error => {
+        swal({
+          title: "Erreur",
+          text: "L'operation a échoué",
+          confirmButtonColor: "#FF0000",
+          type:"warning",
+          button: "OK!",
+        });
+      });
+    this.cleanAddClientModal();
+
   }
 
 }
