bad1770fc84760494dfaccdc232bdee5b457111f
Yessine Borchani <borchaniz96@gmail.com>
2018-03-24 11:47:34 +0100

    add livraison form ready, only confirmation is left

--- src/app/livraison/add-livraison/add-livraison.component.ts
+++ src/app/livraison/add-livraison/add-livraison.component.ts
@@ -9,6 +9,10 @@ import {Fournisseur} from "../../shared/models/fournisseur";
 import {FournisseurService} from "../../shared/services/Fournisseur.service";
 import {Client} from "../../shared/models/client";
 import {ClientService} from "../../shared/services/client.service";
+import {Livraison} from "../../shared/models/livraison";
+import {LivraisonProduit} from "../../shared/models/livraison-produit";
+import {Produit} from "../../shared/models/produit";
+import {ProduitService} from "../../shared/services/produit.service";
 declare let jQuery: any;
 declare let swal:any;
 @Component({
@@ -18,15 +22,45 @@ declare let swal:any;
 })
 export class AddLivraisonComponent implements OnInit {
   clients:Array<Client>;
-  livraison:any;
-  constructor(private clientSevice:ClientService) { }
+  livraison:Livraison;
+  produits:Array<Produit>;
+  constructor(private clientSevice:ClientService, private produitService: ProduitService) { }
   ngOnInit() {
+    this.livraison=new Livraison();
+    this.livraison.produits=new Array<LivraisonProduit>(new LivraisonProduit());
+    this.produitService.getProduits().subscribe(data=>{
+      this.produits=data;
+      this.livraison.produits[0].produit=this.produits[0];
+    });
     this.clientSevice.getClients().subscribe(data=>{
       this.clients=data;
-      this.livraison=new Object();//TODO change to new Livraison as soon as the model is ready
       this.livraison.client=this.clients[0];
     });
   }
+
+  public addProduct(){
+    this.livraison.produits.push(new LivraisonProduit());
+    this.livraison.produits[this.livraison.produits.length-1].produit=this.produits[0];
+  }
+
+  validData():boolean{
+    let b:boolean;
+    b=this.livraison.client!=undefined&&this.livraison.date!=undefined&&this.livraison.montant!=0&&this.livraison.montant!=undefined;
+    for (let p of this.livraison.produits){
+      b=b&&p.produit!=undefined&&p.quantite!=undefined&&p.quantite!=0;
+    }
+    return b;
+  }
+
+  validProdData():boolean{
+    let b:boolean;
+    b=true;
+    for (let p of this.livraison.produits){
+      b=b&&p.produit!=undefined&&p.quantite!=undefined&&p.quantite!=0;
+    }
+    return b;
+  }
+
 
 
 
