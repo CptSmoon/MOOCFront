f3cc782b41c475422b4e86baa404143f9e085838
abbesyassine <abbesyassine4@gmail.com>
2018-03-27 12:19:15 +0100

    change baseUrl

--- src/app/shared/config.ts
+++ src/app/shared/config.ts
@@ -3,10 +3,10 @@ export class Config {
   // public static baseUrl = "https://wsecos.vayetek.com/api";
   // public static baseUrlPlateforme = "https://wsstudentfile.vayetek.com/api";
   // public static baseUrl = "http://137.74.165.25:8082/api";
-  public static baseUrl = 'http://localhost/erp-backend/public/api';
-  // public static baseUrl = 'http://137.74.165.25:8086/api';
+  // public static baseUrl = 'http://localhost/erp-backend/public/api';
+  public static baseUrl = 'http://137.74.165.25:8086/api';
 
-  public static adminUrl = Config.baseUrl + "/admin";
+  public static adminUrl = Config.baseUrl + '/admin';
 
 
 }
