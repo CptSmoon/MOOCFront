bcbefd20fafb6362c01e3b6068fc2fda88b0353b
abbesyassine <abbesyassine4@gmail.com>
2018-04-10 12:15:53 +0100

    fixing bugs

--- src/app/shared/config.ts
+++ src/app/shared/config.ts
@@ -3,8 +3,8 @@ export class Config {
   // public static baseUrl = "https://wsecos.vayetek.com/api";
   // public static baseUrlPlateforme = "https://wsstudentfile.vayetek.com/api";
   // public static baseUrl = "http://137.74.165.25:8082/api";
-  // public static baseUrl = 'http://localhost/erp-backend/public/api';
-  public static baseUrl = 'http://137.74.165.25:8086/api';
+  public static baseUrl = 'http://localhost/erp-backend/public/api';
+  // public static baseUrl = 'http://137.74.165.25:8086/api';
 
   public static adminUrl = Config.baseUrl + '/admin';
 
