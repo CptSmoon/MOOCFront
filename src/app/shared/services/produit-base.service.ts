0d0723f604722fdc61a8ad304bad5df4e0b6f5c2
abbesyassine <abbesyassine4@gmail.com>
2018-04-10 02:08:35 +0100

    fixing bugs type

--- src/app/shared/services/produit-base.service.ts
+++ src/app/shared/services/produit-base.service.ts
@@ -4,7 +4,8 @@ import {Config} from '../config';
 import {HttpClient} from '@angular/common/http';
 import {GenericService} from './generic.service';
 import {Produit_Base} from '../new models/produit_base';
-import {Type} from '../new models/Type';
+import {Type} from '../new models/type';
+
 
 
 @Injectable()
@@ -22,10 +23,11 @@ export class ProduitBaseService extends GenericService {
     return this.http.put(Config.baseUrl + '/produitbase/' + produit_base_id + '/edit', produit_base);
   }
 
-  getTypes():Observable<Array<Type>>{
+  getTypes(): Observable<Array<Type>> {
     return <Observable<Array<Type>>>this.http.get(Config.baseUrl + '/produitbase/types');
   }
-  add(produit:Produit_Base):Observable<Produit_Base>{
+
+  add(produit: Produit_Base): Observable<Produit_Base> {
     return <Observable<Produit_Base>> this.http.post(Config.baseUrl + '/produitbase/add', produit);
   }
 
